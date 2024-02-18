916b65b64d5059fd564930c56ffd7830026d13b8
Bis92 <bis920309@gmail.com>
2024-02-19 02:05:06 +0900

    Featured Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import './globals.css'
 import Hero from '@/components/Hero';
+import FeaturedPosts from '@/components/FeaturedPosts';
 
 export default function HomePage() {
   return <>
     <Hero />
+    <FeaturedPosts />
   </>
 }
