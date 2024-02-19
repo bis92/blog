9474bdf1d6fe6803b39cfaf28fedc6bdf0c3bd6f
Bis92 <bis920309@gmail.com>
2024-02-20 00:44:35 +0900

    Carousel Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import './globals.css'
 import Hero from '@/components/Hero';
 import FeaturedPosts from '@/components/FeaturedPosts';
+import CarouselPosts from '@/components/CarouselPosts';
 
 export default function HomePage() {
   return <>
     <Hero />
     <FeaturedPosts />
+    <CarouselPosts />
   </>
 }
---
+++ src/components/MultiCarousel.tsx
@@ -0,0 +1,44 @@
+'use client'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import PostCard from './PostCard';
+import React from 'react';
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2
+  }
+};
+;
+
+type Props = { 
+  children: React.ReactNode; 
+};
+
+export default function MultiCarousel({ children }: Props) {
+  return (
+    <Carousel 
+      responsive={responsive}
+      infinite
+      autoPlay
+      itemClass='m-2'
+    >
+      {children}
+    </Carousel>
+  );
+}
+
